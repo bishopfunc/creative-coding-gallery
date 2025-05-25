11ae141fdf65743cbf4f94406eab15b80c3030a8
bishopfunc <showyamamoto02@gmail.com>
2025-05-26 02:34:54 +0900

    スケッチ変更

--- src/sketches/sketchA.ts
+++ src/sketches/sketchA.ts
@@ -1,13 +1,15 @@
-// sketches/sketchA.ts
 import p5 from "p5";
+import type { SketchFunc } from "../utils/types";
 
-export const sketchA = (p: p5) => {
+export const sketchA: SketchFunc = (p: p5) => {
   p.setup = () => {
     p.createCanvas(300, 200);
     p.background(255);
   };
+  
   p.draw = () => {
     p.fill(255, 0, 0);
     p.ellipse(p.mouseX, p.mouseY, 50);
   };
+  return p
 };
--- src/sketches/sketchB.ts
+++ src/sketches/sketchB.ts
@@ -1,13 +1,15 @@
-// sketches/sketchB.ts
 import p5 from "p5";
+import type { SketchFunc } from "../utils/types";
 
-export const sketchB = (p: p5) => {
+export const sketchB: SketchFunc = (p: p5) => {
   p.setup = () => {
     p.createCanvas(300, 200);
     p.background(200);
   };
+  
   p.draw = () => {
     p.fill(0, 0, 255);
     p.rect(p.mouseX, p.mouseY, 40, 40);
   };
+  return p;
 };
